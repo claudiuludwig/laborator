8e95b6cbafcd52866c39805cb4b11312d6d9c28d
claudiuludwig <83950771+claudiuludwig@users.noreply.github.com>
2021-10-29 15:31:52 +0300

    Add files via upload

---
+++ Claudiu Ludwig/lab3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using Google.Apis.Drive.v3;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+namespace L03
+{
+    class Program
+    {
+
+        private static DriveService _service;
+        private static string _token;
+        static void Main(string[] args)
+        {
+            Initialize();
+        }
+
+        static void Initialize()
+        {
+            string[] scopes = new string[]{
+                DriveService.Scope.Drive,
+                DriveService.Scope.DriveFile
+            };
+
+            var clientId = "120996325968-029qs3d1oe85fpf08g0kseso5nffmhup.apps.googleusercontent.com";
+            var clientSecret = "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";
+
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = clientId,
+                    ClientSecret = clientSecret
+                },
+                scopes,
+                Environment.UserName,
+                CancellationToken.None,
+
+                new FileDataStore("Daimto.GoogleDrive.Auth.Store")
+            ).Result;
+
+            _service = new DriveService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+
+            _token = credential.Token.AccessToken;
+
+            Console.Write("Token: " + credential.Token.AccessToken);
+
+            GetMyFiles();
+        }
+
+        static void GetMyFiles()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/drive/v3/files?q='root'%20in%20parent");
+            request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + _token);
+
+            using (var response = request.GetResponse())
+            {
+                using (Stream data = response.GetResponseStream())
+                using (var reader = new StreamReader(data))
+                {
+                    string text = reader.ReadToEnd();
+                    var myData = JObject.Parse(text);
+                    foreach (var file in myData["files"])
+                    {
+                        if (file["mimeType"].ToString() != "application/vnd.google-apps.folder")
+                        {
+                            Console.WriteLine("File name: " + file["name"]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
